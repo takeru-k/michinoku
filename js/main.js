e141145059c82617ba6c18af3672f2782df2ea2b
takeru-k <kam41893@gmail.com>
2022-03-18 16:46:17 +0900

    各種修正

--- js/main.js
+++ js/main.js
@@ -34,3 +34,43 @@ $('#slider').vegas({
       { src: './images/top/top-about.jpg'},   
       ],
 });
+
+$(function() {
+  $(window).on('scroll resize', function() {
+    var setHeight = 100;//設定した高さ
+    var wHeight = $(window).height();//ウィンドウの高さ
+    var scrollTop = $(window).scrollTop();//ページの上からスクロールされた分
+    $('.animate').each(function() {
+      var targetPosition = $(this).offset().top;//対象要素と画面上部までの距離
+      if(scrollTop > targetPosition - wHeight + setHeight) {
+        $(this).addClass('show');
+      }
+    })
+  });
+});
+
+$(window).on('load scroll', function(){
+  
+  $('.animated').css('opacity', '0');
+
+  //animatedのclassを持った要素をセレクタに指定
+  var elem = $('.animated');
+
+  elem.each(function () {
+
+    //data属性からアニメーション名を取得
+    var isAnimate = $(this).data('animate');
+    //animated要素に位置を取得
+    var elemOffset = $(this).offset().top;
+    //現在のスクロールポジションを取得
+    var scrollPos = $(window).scrollTop();
+    //ウィンドウの高さを取得
+    var wh = $(window).height();
+
+    //animated要素がウィンドウ内の中央位置にきたら処理
+    if(scrollPos > elemOffset - wh + (wh / 2)){
+      $(this).addClass(isAnimate);
+    }
+  });
+
+});
